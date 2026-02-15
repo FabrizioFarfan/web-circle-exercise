092d879c2dc0d359bcb95bfd41e659a7bfa98d1a
FabrizioFarfan <farfanfabrizio96@gmail.com>
2026-02-15 13:12:27 +0100

    Search functionality finished
    Given an input it returns matching dishes

--- src/components/SearchField/SearchField.jsx
+++ src/components/SearchField/SearchField.jsx
@@ -1,12 +1,17 @@
+import { useRef } from "react";
 import styles from "./SearchField.module.css";
 
-const SearchField = () => {
+const SearchField = ({ setSearchDish }) => {
+  const inputRef = useRef(null);
+
   return (
     <div className={styles.wrapper}>
       <input
+        ref={inputRef}
         className={styles.search}
         placeholder="Filter dishes..."
         type="text"
+        onChange={() => setSearchDish(inputRef.current.value)}
       />
     </div>
   );
