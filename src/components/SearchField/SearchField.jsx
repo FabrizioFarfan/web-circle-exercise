102375efe68cb52328810ba4ef077e045dbc0e39
FabrizioFarfan <farfanfabrizio96@gmail.com>
2026-02-15 12:14:49 +0100

    added style to SearchField

--- src/components/SearchField/SearchField.jsx
+++ src/components/SearchField/SearchField.jsx
@@ -1,12 +1,12 @@
 import styles from "./SearchField.module.css";
 
 const SearchField = () => {
-  
   return (
     <div className={styles.wrapper}>
       <input
+        className={styles.search}
         placeholder="Filter dishes..."
-        type="text"     
+        type="text"
       />
     </div>
   );
